b60afae44cb374b2e70a6589834d20353422a4e3
Michael da Costa Móra <michael.c.mora@gmail.com>
2021-11-13 12:57:53 -0200

    Commit inicial.

---
+++ SpringBoot-helloworld-application/src/test/java/net/javaguides/springboot/Springboothelloworldapplication/SpringBootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.Springboothelloworldapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
